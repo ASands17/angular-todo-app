6d385cf840492ec45fdc3a09a7bdc6ee0df706d1
Amanda Sands <amandajean555@gmail.com>
2022-09-23 09:49:29 -0600

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  //& This below is setting the url path as the default (/)
+  //& Then it renders the component that is declared
+  { path: '', component: TodosComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

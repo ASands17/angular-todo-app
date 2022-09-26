cba28195eedb81e82eb6b4c421edc37c937c157c
Amanda Sands <amandajean555@gmail.com>
2022-09-26 07:28:19 -0600

    Set up basic service

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  todos: Todo[]
+
+  constructor() { }
+
+
+  getAllTodos() {
+    return this.todos
+  }
+
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo)
+  }
+
+  updateTodo(index: number, updatedTodo: Todo) {
+    this.todos[index] = updatedTodo
+  }
+
+  deleteTodo(index: numvber) {
+    this.todos.splice(index, 1)
+  }
+
+
+}
